c64e59e70586ff2997130491e880bade2fe17344
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-11-16 09:44:15 +0100

    multiple files

---
+++ DM6-Nombres-Heureux/code/probabilite.py
@@ -0,0 +1,14 @@
+from heureux import heureux
+
+def probabilite(Nmax):
+    """Compte la probabilite qu'un nombre inferieur à Nmax soit heureux"""
+    return (
+        [
+            heureux(n) for n in range(1, Nmax+1)
+        ].count(True)
+        / Nmax
+    )
+
+
+if __name__ == "__main__":
+    print(probabilite(100))
