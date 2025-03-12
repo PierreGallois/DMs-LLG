b29dc42ae7a0504e03a23be10f032833cb5bf77a
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2025-03-12 17:00:39 +0100

    DM11 prog v2

--- DM11-Equation-de-Pell-Fermat/calc_sol.hs
+++ DM11-Equation-de-Pell-Fermat/calc_sol.hs
@@ -3,13 +3,16 @@
 f :: Double -> Double
 f b = sqrt (1 + 5 * b^2)
 
-test_to :: Double -> [Double]
-test_to n = filter (isNat . f) [1..n]
+test_to :: Double -> [(Double, Double)]
+test_to n = filter (isNat . image) (map (\e -> (e, f e)) [1..n])
 
 -- Ignore after
 isNat :: Double -> Bool
 isNat n = n >= 0 && (n - fromIntegral (truncate n)) <= 10^^(-7)
 
+image :: (a, a) -> a
+image (_, y) = y 
+
 main :: IO()
-main = print (map truncate (test_to 200000000000000))
+main = print (map (\(x, y) -> (truncate x, truncate y)) (test_to 200000000000000))
 -- to debug change truncate to id
