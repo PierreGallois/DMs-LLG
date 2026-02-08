c441fc6b008ba839168183b28355813b8c32b4ec
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2026-02-08 16:36:22 +0100

    code format

--- TE1-DM10-Theoreme-de-Beatty-et-Jeu-de-Wythoff/beatty.py
+++ TE1-DM10-Theoreme-de-Beatty-et-Jeu-de-Wythoff/beatty.py
@@ -1,19 +1,19 @@
 import math
 from itertools import count
 
-phi = (1 + math.sqrt(5))/2
+phi = (1 + math.sqrt(5)) / 2
 a = lambda p: math.floor(p * phi)
 b = lambda p: math.floor(p * phi * phi)
 
-def is_beatty(u,v):
+def is_beatty(u, v):
     p = abs(u - v)
-    return min(u,v) == a(p)
+    return min(u, v) == a(p)
 
-def coup_gagnant(u,v):
-    u,v = min(u,v), max(u,v) # On ordonne u et v
+def coup_gagnant(u, v):
+    u, v = min(u, v), max(u, v) # On ordonne u et v
 
     # On est en situation perdante
-    if is_beatty(u,v):
+    if is_beatty(u, v):
         return (u - 1, v - 1)
     
     # On détermine si u = a(p) ou u = b(p)
